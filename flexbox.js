d3f860f127062be441a6ae3fe23583ed30f821e2
Endurancium <112734900+Endurancium@users.noreply.github.com>
2022-10-14 12:01:29 +0100

    Add files via upload

---
+++ flexbox.js
@@ -0,0 +1,14 @@
+
+    var click=document.getElementsByTagName("a");
+    click.onclick=function(){
+            click.style.color="red"; 
+        };
+
+        function mouseover(){
+            
+        };
+
+        function loading(){
+   prompt("Please note that this site uses cookies for better user experience!    what do you think?", "pls input feedback")
+        }
+
